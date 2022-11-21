2863b38ca3623985caaac3145d0c72f238f48fbc
Lasse Sailer <l.sailer@online.de>
2022-11-21 23:47:24 +0100

    changed style and added working controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,56 +1,28 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = (0..10).map { ('a'..'z').to_a[rand(26)] }.join
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
-    @answer = params[:answer]
+    @letters = params[:letters].split
+    @answer = (params[:answer] || '').upcase
+    @included = included?(@answer, @letters)
+    @english_word = english_word?(@answer)
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(answer)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{answer}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
-
-
-# require 'open-uri'
-# require 'json'
-
-# def generate_grid(grid_size)
-#   # TODO: generate random grid of letters
-#   Array.new(grid_size) { ('A'..'Z').to_a.sample }
-# end
-
-# def included?(guess, grid)
-#   guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
-# end
-
-# def compute_score(attempt, time_taken)
-#   time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
-# end
-
-# def run_game(attempt, grid, start_time, end_time)
-#   # TODO: runs the game and return detailed hash of result (with `:score`, `:message` and `:time` keys)
-#   result = { time: end_time - start_time }
-
-#   score_and_message = score_and_message(attempt, grid, result[:time])
-#   result[:score] = score_and_message.first
-#   result[:message] = score_and_message.last
-
-#   result
-# end
-
-# def score_and_message(attempt, grid, time)
-#   if included?(attempt.upcase, grid)
-#     if english_word?(attempt)
-#       score = compute_score(attempt, time)
-#       [score, "well done"]
-#     else
-#       [0, "not an english word"]
-#     end
-#   else
-#     [0, "not in the grid"]
-#   end
-# end
-
-# def english_word?(word)
-#   response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
-#   json = JSON.parse(response.read)
-#   return json['found']
-# end
